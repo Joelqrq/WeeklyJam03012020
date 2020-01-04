d25e21c07a7ed7d34fe78acb61158af62c49263d
Onnion191 <aleixrius19@gmail.com>
2020-01-04 14:29:11 +0100

    Added player manager and stats handler

---
+++ Assets/Scripts/Static/MathLibrary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathLibrary
+{ 
+
+
+    //Returns Vector representing the direction and magnitude of the jump
+    public static Vector3 CalculateJump(Vector3 UpVector, Vector3 Speed)
+    {
+        return CalculateJumpDirection(UpVector) * CalculateJumpForce(Speed); 
+    }
+
+
+    private static Vector3 CalculateJumpDirection(Vector3 UpVector)
+    {
+        return Vector3.Lerp(UpVector, Vector3.up, Mathf.Abs(Vector3.Dot(UpVector, Vector3.up)) );
+    }
+
+    private static float CalculateJumpForce(Vector3 Speed)
+    {
+        return Mathf.Lerp(PlayerManager.GetMinJump(),PlayerManager.GetMaxJump(), ( Speed.magnitude / PlayerManager.GetMaxSpeed() ) ); 
+    }
+}
